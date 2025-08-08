b0fe1d0eea7dcbb3f81483d75c12b27b935f406a
Yunseong <dev.yunseong@gmail.com>
2025-08-08 20:42:50 +0900

    Add WebSecurityConfig for security configuration and healthcheck access

---
+++ src/main/java/com/wordonline/admin/config/WebSecurityConfig.java
@@ -0,0 +1,39 @@
+package com.wordonline.admin.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@RequiredArgsConstructor
+@EnableWebSecurity
+@Configuration
+@EnableMethodSecurity
+public class WebSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .headers(headers ->
+                        headers
+                                .frameOptions(HeadersConfigurer.FrameOptionsConfig::disable)
+                )
+                .authorizeHttpRequests(authorizeRequests -> {
+                            authorizeRequests
+                                    .requestMatchers(
+                                            "/healthcheck")
+                                    .permitAll()
+                                    .anyRequest().authenticated();
+                        }
+
+                )
+                .csrf(AbstractHttpConfigurer::disable);
+
+        return http.build();
+    }
+}
