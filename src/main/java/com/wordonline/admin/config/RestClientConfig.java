6c4baa26cd6e802ba965b9e5f56c0d45ae5bdd30
Yunseong <dev.yunseong@gmail.com>
2025-12-14 10:40:18 +0900

    Add JWT configuration and update GameServerClient to use RestClient

---
+++ src/main/java/com/wordonline/admin/config/RestClientConfig.java
@@ -0,0 +1,35 @@
+package com.wordonline.admin.config;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestClient;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Configuration
+public class RestClientConfig {
+
+    @Value("${word-online.jwt.path}")
+    private String jwtFilePath;
+
+    @Bean
+    public RestClient.Builder builder() {
+        return RestClient.builder().defaultHeader("Authorization", loadJwt());
+    }
+
+    private String loadJwt() {
+        try {
+            return Files.readString(Path.of(jwtFilePath)).trim();
+        } catch (IOException e) {
+            log.error("Fail to Load jwt {}", jwtFilePath, e);
+            return "";
+        }
+    }
+}
